ee073b4f228e1a7e06e94b0bd2048b401eb7937c
Jacky <jack@mrqu.net>
2022-11-29 20:32:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,11 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+# for wr703n with 16m
+sed -i '227s/4mlzma/16mlzma/g' target/linux/ath79/image/tiny-tp-link.mk
+sed -i 's/0x20000 0x3d0000/0x20000 0xfd0000/g' target/linux/ath79/dts/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi
+sed -i 's/0x3f0000 0x10000/0xff0000 0x10000/g' target/linux/ath79/dts/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi
+
 # make defconfig
 sed -i 's/^[ \t]*//g' ./.config
 make defconfig
